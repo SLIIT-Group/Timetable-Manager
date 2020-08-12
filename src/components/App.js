40a36fe92da47caf6a8bae08b5aa1fce893ade68
imalK96 <imalkrulez@gmail.com>
2020-08-12 19:56:19 +0530

    Inital commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SideNav from './Navigation/SideNav';
+const App = () => {
+  return (
+    <div className='app'>
+      <SideNav></SideNav>
+    </div>
+  );
+};
+
+export default App;
