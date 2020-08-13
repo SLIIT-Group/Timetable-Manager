aae3ef8f85fe403f6b92973dc2da0cd3816db990
imalK96 <imalkrulez@gmail.com>
2020-08-13 18:50:41 +0530

    Added expansion panels for time manager

--- src/components/TimeManager/TimeManager.js
+++ src/components/TimeManager/TimeManager.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import TimeSlot from './TimeSlot';
+import AllocateWorkingDays from './AllocateWorkingDays';
+import NoOfWorkingDays from './NoOfWorkingDays';
 
 function TimeManager() {
-  return <div style={{ textAlign: 'center' }}>Time Manager</div>;
+  return (
+    <div>
+      <NoOfWorkingDays></NoOfWorkingDays>
+      <AllocateWorkingDays></AllocateWorkingDays>
+      <TimeSlot></TimeSlot>
+    </div>
+  );
 }
 
 export default TimeManager;
