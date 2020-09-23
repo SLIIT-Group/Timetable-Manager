0895029673a7531e2831d0460e36311cc9a08f92
Nuwanga <thnuwanga@gmail.com>
2020-09-24 01:22:24 +0530

    not available done

---
+++ src/components/Customize/Panels/Parallel.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react';
+import { makeStyles, emphasize } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import StudentForm from "../SubComponents/StudentForm";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    margin: '10px',
+    marginRight: '10px',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function Parallel() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+
+  return (
+    <div className={classes.root}>
+      <Accordion
+          style={
+            expanded
+                ? { backgroundColor: '#f5f5f5' }
+                : { backgroundColor: '#3f51b5', color: '#fff' }
+          }
+        expanded={expanded === 'panel1'}
+        onChange={handleChange('panel1')}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls='panel1bh-content'
+          id='panel1bh-header'
+        >
+          <Typography className={classes.heading}>Parallel Sessions</Typography>
+
+        </AccordionSummary>
+        <AccordionDetails>
+         <StudentForm noBackBtn={true}/>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
---
+++ src/components/Customize/SubComponents/Session.js
@@ -0,0 +1,79 @@
+import React, {useEffect, useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {Col} from "reactstrap";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import axios from "axios";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    margin: '10px',
+    marginRight: '10px',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function Session({key, setKey}) {
+  const classes = useStyles();
+
+  const [lecList, setLecList] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/sessions")
+        .then((response) => {
+          setLecList(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+  }, []);
+
+
+  return (
+
+      <>
+        <div className="col-md-12 row px-5">
+
+          <div className="row col-md-12">
+            <Col sm="6 pb-0">
+              <label className="mt-3 h5 font-weight-bold">Session :</label>
+            </Col>
+            <div className="input-field col s6 d-flex flex-column">
+              <FormControl variant="outlined" className={classes.formControl}>
+                <InputLabel id="demo-simple-select-outlined-label px-4">Select</InputLabel>
+                <Select
+                    labelId="demo-simple-select-outlined-label"
+                    id="demo-simple-select-outlined"
+                    value={key}
+                    onChange={(event) => setKey(event.target.value)}
+                    label="lKey"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  {lecList.map((item) => (
+                      <MenuItem value={item._id}>{item.subject+" "+item.tag
+                      +" | Sub/Group:"+item.groupId}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
+          </div>
+
+        </div>
+      </>
+
+
+  );
+}
